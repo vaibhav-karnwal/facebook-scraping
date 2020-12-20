e591a2fa525655e8398cc1d17240796b7f3e8c37
vaibhav-karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2020-12-21 00:54:23 +0530

    Update Scrape_particular_date_data.py

--- Scrape_particular_date_data.py
+++ Scrape_particular_date_data.py
@@ -3,9 +3,10 @@ import pandas as pd
 from credential import input_date, input_time
 
 # making data frame from csv file  
-data = pd.read_csv("facebook_scraped_post.csv") 
-print(data) 
+df = pd.read_csv("facebook_scraped_post.csv") 
+
 # replacing blank spaces with '_'  
-data.columns =[column.replace(" ", "_") for column in data.columns] 
-  
-print(data)
+df.columns =[column.replace(" ", "_") for column in df.columns] 
+newdf=df.query('dates=="16 December " &Time==" 12:23"')
+
+print(newdf)
